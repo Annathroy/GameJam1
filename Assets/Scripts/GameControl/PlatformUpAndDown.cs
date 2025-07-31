ba953b3b840d91fbc0956c04dd6e66bb46908749
Kristijan Turić <kristijan.turic1@gmail.com>
2025-07-31 18:08:05 +0200

    Add basic layout

--- Assets/Scripts/GameControl/PlatformUpAndDown.cs
+++ Assets/Scripts/GameControl/PlatformUpAndDown.cs
@@ -1,16 +1,19 @@
+using System;
 using UnityEngine;
 
 public class PlatformUpAndDown : MonoBehaviour
 {
-    
-
     private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-
-        float hover = startPosition.y - 2f + Mathf.Sin(Time.time * 1f) * 1.5f;
+        float hover = startPosition.y - 1f + Mathf.Sin(Time.time * 1f) * 1.5f;
         transform.position = new Vector2(transform.position.x, hover);
     }
     private void OnCollisionEnter2D(Collision2D collision)
